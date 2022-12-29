2609df76064113f4a6d7987fdbd1326d568ea710
Matthew Pounsett <matt@conundrum.com>
2022-12-29 12:24:31 -0500

    fixing HTML layout issues

--- __init__.py
+++ __init__.py
@@ -53,6 +53,10 @@ class Lightbox(Directive):
         "alt": str,
         "caption": str,
         "align": align,
+        "div_class": str,
+        "image_class": str,
+        "a_class": str,
+        "caption_class": str,
     }
 
     final_argument_whitespace = False
@@ -69,35 +73,49 @@ class Lightbox(Directive):
 
         uuid = str(uuid4())
 
-        alt = self.options.get("alt", None)
         align = self.options.get("align", "left")
 
-        if alt is not None:
-            alt_text = "{} (click to view large image)".format(alt)
-        else:
+        div_class = self.options.get("div_class", None)
+        if div_class is None:
+            div_class = ""
+        if div_class and align:
+            div_class = f'{div_class} align-{align}'
+        elif align:
+            div_class = f'align-{align}'
+
+        image_class = self.options.get("image_class", None)
+        if image_class is None:
+            image_class = ""
+
+        a_class = self.options.get("a_class", None)
+        if a_class is None:
+            a_class = ""
+
+        caption_class = self.options.get("caption_class", None)
+        if caption_class is None:
+            caption_class = ""
+
+        alt_text = self.options.get("alt", None)
+        if alt_text is None:
             alt_text = "Click to view large image"
 
         caption = self.options.get("caption", None)
-        if caption is not None:
-            caption_block = (
-                """<p class="align-{}">{} (click to view large """
-                """image)</p>"""
-            ).format(align, caption)
-        else:
-            caption_block = (
-                """<p class="align-{}">Click to view large image</p>"""
-            ).format(align)
+        if caption is None:
+            caption = 'Click to view large image'
 
         block = (
-            """<div class="lightbox-block align-{4}">"""
-            """<a href="#{0}" title="{3}">"""
-            """<img src="{1}" alt="{3}" class="align-{4}" /></a>"""
-            """<a href="#_" class="lightbox" id="{0}" title="Click to """
-            """close">"""
-            """<img alt="Click to close" src="{2}" /></a>{5}</div>"""
-            """<div class="lightbox-divider">"""
-            """</div>"""
-        ).format(uuid, thumb, large, alt_text, align, caption_block)
+            f'<div class="lightbox-block {div_class}">'
+            f'  <a href="#{uuid}" title="{alt_text}">'
+            f'    <img src="{thumb}" alt="{alt_text}" class="{image_class}"/>'
+            f'  </a>'
+            f'  <a href="#_" class="lightbox" id="{uuid}" '
+            f'     title="Click to close">'
+            f'    <img alt="Click to close" src="{large}"/>'
+            f'  </a>'
+            f'  <p class="lightbox-caption {caption_class}">{caption}</p>'
+            f'  <div class="lightbox-divider"></div>'
+            f'</div>'
+        )
         return [nodes.raw("", block, format="html")]
 
 
